773ea4f2fae54242f68aff6cc28d0bee1677a3c4
GIOVANNI GENNA s304684 <s304684@studenti.polito.it>
2022-05-27 17:14:21 +0200

    DCF for MVG

--- Gaussian_Classifiers/MVG.py
+++ Gaussian_Classifiers/MVG.py
@@ -4,6 +4,7 @@ import numpy
 import scipy
 import scipy.special
 import pylab
+import matplotlib
 
 def mean(class_identifier, training_data, training_labels):
     Dc = training_data[:, training_labels == class_identifier]
@@ -27,12 +28,35 @@ def logpdf_GAU_ND(training_data, mean, covariance_matrix):
        res= const - 0.5 * numpy.dot((x-mean).T, numpy.dot(P, (x-mean)))
        l_x.append(res)
      
-    return mrow(numpy.array(l_x));
+    return mrow(numpy.array(l_x))
+
+
+def conf_matrix(llratio, labs, pr, C_fn, C_fp):
+    
+    # computing c* comparing the llr with a threshold t
+    t= - numpy.log((pr*C_fn)/((1-pr)*C_fp))
+    
+    C_aster=numpy.zeros([llratio.shape[0],], dtype= int)
+    
+    for i in range(llratio.shape[0]):
+        if llratio[i]>t:
+            C_aster[i]= 1
+        else:
+            C_aster[i]= 0
+            
+    # computing the confusion matrix, comparing labels and c*    
+    conf_matr= numpy.zeros([2,2], dtype= int)
+    for j in range(2):
+        for i in range(2):
+            conf_matr[j,i]= ((C_aster==j) * (labs==i)).sum()
+    return conf_matr
 
 if __name__=='__main__':
+    prior= 4/9
     data = load_data()
     training_data = data[0]
     training_labels = data[1]
+    
 
     training_data = computePCA(training_data, 9)
 
@@ -79,8 +103,8 @@ if __name__=='__main__':
 
     log_scores = log_scores[1:, :]
     # We assume that the prior probability of each class is 1/2
-    Pc_0 = numpy.log(5/9)
-    Pc_1 = numpy.log(4/9)
+    Pc_0 = numpy.log(1-prior)
+    Pc_1 = numpy.log(prior)
     logSJoint_0 = log_scores[:, 0] + Pc_0
     logSJoint_1 = log_scores[:, 1] + Pc_1
     logSJoint = numpy.vstack((logSJoint_0, logSJoint_1)).T
@@ -102,8 +126,8 @@ if __name__=='__main__':
             confusion[j,i] = ((Predicted_labels==j) * (real_labels==i)).sum()
  
    
-    FNR = confusion[0,1]/(confusion[0,1]+ confusion[1,1])
-    FPR = confusion[1,0]/(confusion[1,0]+ confusion[0,0])
+    FNR_ = confusion[0,1]/(confusion[0,1]+ confusion[1,1])
+    FPR_ = confusion[1,0]/(confusion[1,0]+ confusion[0,0])
     err = (confusion[0,1]+confusion[1,0])/(confusion[0,1]+ confusion[1,1]+confusion[1,0]+ confusion[0,0])
     acc = 1-err
     print("Model accuracy:", round(acc*100, 3), "%")
@@ -139,5 +163,82 @@ if __name__=='__main__':
     pylab.plot(FPR, TPR)
     pylab.show()
 
+    #compute the normalized DCF of our model
+    Cost_matrix= numpy.zeros([2,2], dtype= int)
+    Cfn= 1
+    Cfp= 1
+    Cost_matrix[0,1]= Cfn
+    Cost_matrix[1,0]= Cfp
+    confusion__matrix = conf_matrix(llr, real_labels, prior, Cfn, Cfp)
+    FNR=  confusion__matrix[0,1] / ( confusion__matrix[0,1] +  confusion__matrix[1,1])
+    FPR=  confusion__matrix[1,0] / ( confusion__matrix[1,0] +  confusion__matrix[0,0])
+    Bemp= prior*Cfn*FNR + (1-prior)*Cfp*FPR
+    Bdummy= min(prior*Cfn, (1-prior)*Cfp) 
+    normDCF= Bemp/Bdummy
+    print(normDCF)
+    
+ 
+    #compute the minimum normalized DCF for our model
+    Bempirical= numpy.zeros(thresholds.size)
+    for idx, t in enumerate(thresholds):
+        Pred= numpy.int32(llr > t)
+        Conf= numpy.zeros((2, 2))
+        for j in range(2):
+            for i in range(2):
+                Conf[j, i]= ((Pred== j) * (real_labels==i)).sum()
+        FNR__= Conf[0,1] / (Conf[0,1] + Conf[1,1])
+        FPR__= Conf[1,0] / (Conf[1,0] + Conf[0,0])
+        Bempirical[idx]= prior*Cfn*FNR__ + (1-prior)*Cfp*FPR__
+   
+    Bemp_min= Bempirical.min()   
+    min_normDCF= Bemp_min/Bdummy
+    print(min_normDCF)
     
     
+    
+    # Compute the Bayes error plot for our recognizer. Consider values of p˜ ranging, for example, from -3
+    # to +3. You can generate linearly spaced values (21 is the number of points we evaluate the DCF at in the example, i.e. the number of values of p˜
+    # we consider). For each value p˜, compute the corresponding effective prior
+    
+    effPriorLogOdds = numpy.linspace(-3, 3, 21) 
+    normalizedDCF= numpy.zeros(21)
+    minDCF= numpy.zeros(21)
+    
+    for  index in range(21):
+        eff_prior= 1/ (1 + numpy.exp( - effPriorLogOdds[index]))
+        #compute the actual DCF
+        Bdummy= min(eff_prior*Cfn, (1-eff_prior)*Cfp)
+        c_m= conf_matrix(llr, real_labels, eff_prior, Cfn, Cfp)
+        FNR__=  c_m[0,1] / ( c_m[0,1] +  c_m[1,1])
+        FPR__=  c_m[1,0] / (  c_m[1,0] +  c_m[0,0])
+        B_emp= eff_prior*Cfn*FNR__ + (1-eff_prior)*Cfp*FPR__
+        normBemp= B_emp/Bdummy
+        normalizedDCF[index]= normBemp
+        
+        #compute the min DCF
+        Bemp_= numpy.zeros(thresholds.size)
+        for idx, t in enumerate(thresholds):
+            Pred= numpy.int32(llr > t)
+            Conf= numpy.zeros((2, 2))
+            for j in range(2):
+                for i in range(2):
+                    Conf[j, i]= ((Pred== j) * (real_labels==i)).sum()
+            FNR__= Conf[0,1] / (Conf[0,1] + Conf[1,1])
+            FPR__= Conf[1,0] / (Conf[1,0] + Conf[0,0])
+            Bemp_[idx]= eff_prior*Cfn*FNR__ + (1-eff_prior)*Cfp*FPR__
+             
+       
+        Bemp_min= Bemp_.min()   
+        
+        minDCF[index]= Bemp_min/Bdummy
+        
+        
+    matplotlib.pyplot.xlabel('log(π/(1-π))')
+    matplotlib.pyplot.ylabel('DCF')
+    matplotlib.pyplot.title('Bayes error plot')
+    matplotlib.pyplot.plot(effPriorLogOdds, normalizedDCF, color='r',  label= 'DCF')
+    matplotlib.pyplot.plot(effPriorLogOdds, minDCF, color='b', label= 'min DCF')
+    matplotlib.pyplot.ylim([0, 1.1])
+    matplotlib.pyplot.xlim([-3, 3])
+    matplotlib.pyplot.legend()
+    matplotlib.pyplot.show()
